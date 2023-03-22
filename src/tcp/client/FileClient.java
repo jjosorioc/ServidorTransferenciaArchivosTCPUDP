35de17ffd8f213d0b3989a32778787bb615ed610
Juan José Osorio <j.osorioc@uniandes.edu.co>
2023-03-22 16:03:01 -0500

    Se comunica el nombre del archivo

--- src/tcp/client/FileClient.java
+++ src/tcp/client/FileClient.java
@@ -1,27 +1,34 @@
 package tcp.client;
 
 import java.io.*;
-import java.net.*;
+
+
+import java.net.Socket;
+import java.util.Scanner;
 
 public class FileClient {
-    public static void main(String[] args) throws Exception {
-        Socket clientSocket = new Socket("localhost", 5000);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter file name: ");
+        String fileName = scanner.nextLine();
 
-        System.out.println("Connected to server: " + clientSocket.getInetAddress().getHostName());
+        try (Socket socket = new Socket("localhost", 8000)) {
+            // Send file name to server
+            OutputStream outputStream = socket.getOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+            dataOutputStream.writeUTF(fileName);
 
-        InputStream inputStream = clientSocket.getInputStream();
-        FileOutputStream fileOutputStream = new FileOutputStream("receivedFile.txt");
-
-        byte[] buffer = new byte[1024];
-        int bytesRead = 0;
-
-        while ((bytesRead = inputStream.read(buffer)) != -1) {
-            fileOutputStream.write(buffer, 0, bytesRead);
+            // Receive file from server
+            InputStream inputStream = socket.getInputStream();
+            FileOutputStream fileOutputStream = new FileOutputStream("./arrival/" + fileName);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, bytesRead);
+            }
+            System.out.println("File received successfully.");
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
         }
-
-        System.out.println("File received.");
-        fileOutputStream.close();
-        inputStream.close();
-        clientSocket.close();
     }
 }
--- src/tcp/server/FileServer.java
+++ src/tcp/server/FileServer.java
@@ -1,29 +1,41 @@
 package tcp.server;
 import java.io.*;
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class FileServer {
-    public static void main(String[] args) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(5000);
-        System.out.println("Server waiting for connection...");
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8000)) {
+            System.out.println("Server started.");
 
-        Socket clientSocket = serverSocket.accept();
-        System.out.println("Client connected: " + clientSocket.getInetAddress().getHostName());
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected: " + socket.getInetAddress().getHostName());
 
-        FileInputStream fileInputStream = new FileInputStream("./filesFolder/100MB.txt");
-        OutputStream outputStream = clientSocket.getOutputStream();
+                // Receive file name from client
+                InputStream inputStream = socket.getInputStream();
+                DataInputStream dataInputStream = new DataInputStream(inputStream);
+                String fileName = dataInputStream.readUTF();
+                System.out.println("Client requested file: " + fileName);
 
-        byte[] buffer = new byte[1024];
-        int bytesRead = 0;
-
-        while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-            outputStream.write(buffer, 0, bytesRead);
+                // Send file to client
+                File file = new File("./filesFolder/"+fileName);
+                if (file.exists()) {
+                    OutputStream outputStream = socket.getOutputStream();
+                    FileInputStream fileInputStream = new FileInputStream(file);
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+                    System.out.println("File sent successfully.");
+                } else {
+                    System.out.println("File not found.");
+                }
+                socket.close();
+            }
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
         }
-
-        System.out.println("File sent.");
-        outputStream.close();
-        fileInputStream.close();
-        clientSocket.close();
-        serverSocket.close();
     }
 }
