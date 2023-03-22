73727a75a41f65c79a9960a875ab3592d0ce19c0
Juan José Osorio <j.osorioc@uniandes.edu.co>
2023-03-22 16:37:23 -0500

    Ya se comunican a través de otro puerto

--- src/tcp/client/FileClient.java
+++ src/tcp/client/FileClient.java
@@ -2,7 +2,6 @@ package tcp.client;
 
 import java.io.*;
 
-
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -12,7 +11,7 @@ public class FileClient {
         System.out.print("Enter file name: ");
         String fileName = scanner.nextLine();
 
-        try (Socket socket = new Socket("localhost", 8000)) {
+        try (Socket socket = new Socket("localhost", 5000)) {
             // Send file name to server
             OutputStream outputStream = socket.getOutputStream();
             DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
@@ -26,9 +25,22 @@ public class FileClient {
             while ((bytesRead = inputStream.read(buffer)) != -1) {
                 fileOutputStream.write(buffer, 0, bytesRead);
             }
-            System.out.println("File received successfully.");
+            fileOutputStream.flush();
+            fileOutputStream.close();
+            dataOutputStream.close();
+            outputStream.close();
+            socket.close();
+
+            Socket socket2 = new Socket("localhost", 8000);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket2.getInputStream()));
+            String confirmation = in.readLine();
+            System.out.println(confirmation);
+            in.close();
+
         } catch (IOException ex) {
-            System.out.println("Error: " + ex.getMessage());
+            System.out.println("Error Client: " + ex.getMessage());
+            ex.printStackTrace();
         }
     }
 }
--- src/tcp/server/FileServer.java
+++ src/tcp/server/FileServer.java
@@ -1,11 +1,13 @@
 package tcp.server;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class FileServer {
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(8000)) {
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+            ServerSocket communication = new ServerSocket(8000);
             System.out.println("Server started.");
 
             while (true) {
@@ -19,23 +21,42 @@ public class FileServer {
                 System.out.println("Client requested file: " + fileName);
 
                 // Send file to client
-                File file = new File("./filesFolder/"+fileName);
+                File file = new File("./filesFolder/" + fileName);
                 if (file.exists()) {
                     OutputStream outputStream = socket.getOutputStream();
                     FileInputStream fileInputStream = new FileInputStream(file);
+
                     byte[] buffer = new byte[1024];
                     int bytesRead;
                     while ((bytesRead = fileInputStream.read(buffer)) != -1) {
                         outputStream.write(buffer, 0, bytesRead);
                     }
-                    System.out.println("File sent successfully.");
+
+                    // Close streams
+                    dataInputStream.close();
+                    inputStream.close();
+                    System.out.println("Server: File sent successfully.");
+                    outputStream.close();
+                    fileInputStream.close();
+                    socket.close();
+
+                    
+                    Socket socket2 = communication.accept();
+
+                    PrintWriter out = new PrintWriter(socket2.getOutputStream(), true);
+
+                    out.println("COM: File sent successfully.");
+                    out.flush();
+                    out.close();
+
+                    
                 } else {
                     System.out.println("File not found.");
                 }
-                socket.close();
+
             }
         } catch (IOException ex) {
-            System.out.println("Error: " + ex.getMessage());
+            System.out.println("Error Server: " + ex.getMessage());
         }
     }
 }
